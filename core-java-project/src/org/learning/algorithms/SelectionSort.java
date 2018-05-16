ef04fbc81fd7ccf9dfe087c0e25eaa9252078a3c
Hitesh Lad <ladhitesh@gmail.com>
2018-05-16 22:09:05 +0530

    Selection sort

---
+++ core-java-project/src/org/learning/algorithms/SelectionSort.java
@@ -0,0 +1,26 @@
+package org.learning.algorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+
+		int[] unsorted = { 2, 1, 4, 3 };
+		// comparison sort algorithm
+
+		int[] sorted = new SelectionSort().sort(unsorted);
+		System.out.println(Arrays.toString(sorted));
+
+	}
+
+	private int[] sort(int[] unsorted) {
+		int[] sorted = unsorted;
+		
+		
+		
+		
+		return sorted;
+	}
+
+}
