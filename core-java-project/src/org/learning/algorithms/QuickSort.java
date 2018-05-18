1d028a16bc3f82d22c4e41ada964bfe7f20c1b34
Hitesh Lad <ladhitesh@gmail.com>
2018-05-18 09:47:33 +0530

    Quicksort

---
+++ core-java-project/src/org/learning/algorithms/QuickSort.java
@@ -0,0 +1,57 @@
+package org.learning.algorithms;
+
+import java.util.Arrays;
+
+public class QuickSort {
+	
+	public static void main(String[] args) {
+		int[] unsorted = {2,1,4,3};
+		
+		int[] sorted = new QuickSort().sort(unsorted);
+		
+		System.out.println(Arrays.toString(sorted));
+	}
+
+	private int[] sort(int[] unsorted) {
+		
+		quickSort(unsorted, 0, unsorted.length-1);
+		int[] sorted = unsorted;
+		return sorted;
+	}
+	
+	private void quickSort(int[] input,int start, int end ){
+		
+		if(end <= start)
+			return;
+		int pivotIndex = partition(input, start, end);
+		quickSort(input,start, pivotIndex -1 );
+		quickSort(input, pivotIndex, end);
+		
+	}
+	
+	private int partition(int[] input,int start, int end){
+		System.out.println("start:"+start+",end:"+end +",input:"+Arrays.toString(input));
+		
+		int pivot = input[start]; //pivot value
+		
+		int pivotIndex = start + 1; //pivot index
+		
+		//start comparison after pivot index, move smaller to left of pivot index
+		for (int j = start + 1; j <= end; j++) {
+			if(input[j]<pivot){
+				swap(input, pivotIndex, j);
+				pivotIndex++;
+			}
+		}
+		swap(input, start , pivotIndex-1);
+		System.out.println("pivotIndex:"+pivotIndex+",input:"+Arrays.toString(input));
+		return pivotIndex-1;
+	}
+	
+	public void swap(int[] input, int first, int second){
+		int temp = input[second];
+		input[second] = input[first];
+		input[first] = temp;
+	}
+
+}
