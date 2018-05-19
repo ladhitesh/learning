4bda50e1aea6e53dca5f87b41033e9ee3a3889cb
Hitesh Lad <ladhitesh@gmail.com>
2018-05-19 12:50:42 +0530

    HeapSort

---
+++ core-java-project/src/org/learning/algorithms/HeapSort.java
@@ -0,0 +1,70 @@
+package org.learning.algorithms;
+
+import java.util.Arrays;
+
+public class HeapSort {
+	//heapsort visualization - https://www.cs.usfca.edu/~galles/visualization/HeapSort.html
+	
+	public static void main(String[] args) {
+		int[] unsorted = { 6, 11, 11, 2, 2, 1, 4, 3, 9 };
+		
+		int[] sorted = new HeapSort().sort(unsorted);
+		System.out.println(Arrays.toString(sorted));
+	}
+
+	private int[] sort(int[] unsorted) {
+		
+		//binary heap is complete binary tree. all levels are full except last level
+		//all leaves are from left side of tree
+		//heap is stored in array at level order.
+		//starting from root at index 0, then 1st level nodes, 2nd level nodes
+		//all leaves towards end of array after all nodes
+		
+		//heapify initial array
+		//start from botom level to top level
+		int n = unsorted.length; //no. of elements
+		for (int i = n/2 -1 ; i >= 0; i--) {
+			//for each node level, heapify
+			heapify(unsorted, n, i);
+		}
+		
+		//remove max from root and reheapify
+		for (int i = n-1 ; i >=0; i --) {
+			swap(unsorted,0,i);
+			heapify(unsorted,i,0);
+		}
+		
+		int[] sorted = unsorted;
+		return sorted;
+	}
+
+	private void heapify(int[] unsorted, int maxElementsInHeap, int i) {
+		
+		int max = i;
+		int left = (i*2)+1;
+		int right = (i*2)+2;
+		
+		if(left < maxElementsInHeap && unsorted[left]>unsorted[max]){
+			max = left;
+		}
+		
+		if(right < maxElementsInHeap && unsorted[right]>unsorted[max]){
+			max = right;
+		}
+		
+		//swap if there is change in index holding max value
+		if(max != i){
+			swap(unsorted,max,i);
+			heapify(unsorted,maxElementsInHeap,max);
+		}
+	}
+	
+	public void swap(int[] input, int first, int second){
+		int temp = input[second];
+		input[second] = input[first];
+		input[first] = temp;
+	}
+	
+	
+
+}
