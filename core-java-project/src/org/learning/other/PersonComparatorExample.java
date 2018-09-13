da20013a1185b4a57616b2b257d69089b13448b1
Hitesh Lad <ladhitesh@gmail.com>
2018-09-13 12:37:16 +0530

    java 8 comparator.comparing

--- core-java-project/src/org/learning/other/PersonComparatorExample.java
+++ core-java-project/src/org/learning/other/PersonComparatorExample.java
@@ -4,9 +4,13 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class PersonComparatorExample {
 	
@@ -16,20 +20,39 @@ public class PersonComparatorExample {
 		Person p1 = new Person("Hitesh", 37);
 		Person p2 = new Person("Mitesh", 36);
 		Person p3 = new Person("Jia", 3);
-		Person p4 = new Person("Aarav", 2);
-		List<Person> personList = new ArrayList<>();
-		personList.add(p1);
-		personList.add(p2);
-		personList.add(p3);
-		personList.add(p4);
+		Person p4 = new Person("Aarav", 3);
+		Person p5 = new Person("Aarav", 2);
+		List<Person> unsortedPersonList = new ArrayList<>();
+		unsortedPersonList.add(p1);
+		unsortedPersonList.add(p2);
+		unsortedPersonList.add(p3);
+		unsortedPersonList.add(p4);
+		unsortedPersonList.add(p5);
 		
 		
 		PersonComparator<Integer> ageComparator = new PersonComparator<Integer>(Integer.class, "getAge");
 		PersonComparator<String> nameComparator = new PersonComparator<String>(String.class, "getName");
-		Collections.sort(personList,ageComparator);
-		System.out.println(personList);
-		Collections.sort(personList,nameComparator);
-		System.out.println(personList);
+		
+		//// Java 8 example //////
+		Comparator<Person> personAgeComparator = Comparator.comparing(Person::getAge);
+		Comparator<Person> personNameComparator = Comparator.comparing(Person::getName);
+		Comparator<Person> personNameAgeComparator = Comparator.comparing(Person::getName)
+														.thenComparing(Person::getAge);
+		
+		
+		Map<String,Comparator> comparatorsMap = new TreeMap<>();
+		comparatorsMap.put("methodhandle-age",ageComparator);
+		comparatorsMap.put("methodhandle-name",nameComparator);
+		comparatorsMap.put("java8-age",personAgeComparator);
+		comparatorsMap.put("java8-name",personNameComparator);
+		comparatorsMap.put("java8-name-age",personNameAgeComparator);
+		
+		comparatorsMap.forEach( (comparatorName, comparator) -> {
+			List<Person> sorted = new ArrayList<>(unsortedPersonList);
+			Collections.sort(sorted,comparator);
+			System.out.println(comparatorName+ ":" + sorted);
+		});
+		
 		
 		System.out.println("Done");
 	}
